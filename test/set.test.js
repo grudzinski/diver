27d9e5d644b75521f5dd987acdce80f798732457
Roman Grudzinski <grudzinski@gmail.com>
2015-04-05 17:19:05 +0300

    Add more tests

--- test/set.test.js
+++ test/set.test.js
@@ -10,11 +10,20 @@ describe('set', function() {
 
 		var testObject = {};
 
-		debugger;
-
 		set(testObject, ['x', 'y', 'z'], 'test');
 
 		assert.deepEqual(testObject, { x: { y: { z: 'test' } } });
+
+	});
+
+	it('works 2', function() {
+
+		var testObject = {};
+
+		set(testObject, ['x', 'y', 'z'], 'test');
+		set(testObject, ['x', 'y', 'q'], 'test2');
+
+		assert.deepEqual(testObject, { x: { y: { z: 'test', q: 'test2' } } });
 
 	});
 
